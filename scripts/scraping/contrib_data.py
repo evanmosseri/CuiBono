50801f2314ad1622c0b322c5ba2c973d81954ae9
Evan Mosseri <evan.mosseri@gmail.com>
2016-07-04 20:48:49 -0500

    reworking data files

--- scripts/scraping/contrib_data.py
+++ scripts/scraping/contrib_data.py
@@ -1,99 +1,57 @@
 import pandas as pd
 import csv
 from pprint import pprint
-from scripts.utils import *
+# from scripts.utils import *
 import sys
 import numpy as np
 import re
 import glob
+import argparse
+from utils import *
+
 # data_file = csv.DictReader(open("../../data/cont_ss.csv"))
-data_dir = "../../data/texas_ethics_commision"
-dat_file = pd.read_csv("{}/cont_ss.csv".format(data_dir),low_memory=False)
-
-dat_file = pd.read_csv("../../data/cont_ss.csv",low_memory=False)
-
-org_names =dat_file["filerName"].tolist()
-
-def extract_filer_name(org_name):
-    reversed = False
-    if "Texans For" in org_name.title():
-        org_name = org_name.replace("Texans for ","")
-        org_name = org_name.replace("Texans for ".upper(),"")
-        reversed = True
-    if "Friends Of" in org_name.title():
-        org_name = org_name.replace("Friends of ","")
-        org_name = org_name.replace("Friends Of ","")
-        org_name = org_name.replace("Friends Of ".upper(),"")
-        reversed = True
-    if "Political Action Committee" in org_name.title():
-        org_name = org_name.replace("Political Action Committee","")
-        reversed = True
-    if "Committee To Elect" in org_name.title():
-        org_name = org_name.replace("Committee to Elect ","")
-    if "Committee" in org_name:
-        org_name = org_name.replace(" Committee","")
-        reversed = True
-    if "The Coalition To Elect" in org_name.title():
-        org_name = org_name.replace("The Coalition to Elect","")
-    if "Coalition To Elect" in org_name.title():
-        org_name = org_name.replace("Coalition to Elect","")
-    if "For Texas Supreme Court" in org_name.title():
-        org_name = org_name.replace(" For Texas Supreme Court","")
-        org_name = org_name.replace("For Texas Supreme Court".upper(),"")
-        reversed = True
-    if "For Texas Supreme Court" in org_name.title():
-        org_name = org_name.replace("for Texas Supreme Court","")
-        reversed = True
-    if "For Texas" in org_name.title():
-        org_name = re.sub(r"(F|f)or.*","",org_name)
-        org_name = org_name.replace("For Texas","")
-        org_name = org_name.replace("for Texas","")
-        reversed = True
-    if "Justice" in org_name.title():
-        org_name = org_name.replace("Justice","")
-        reversed = True
-    if "For Supreme Court" in org_name.title():
-        org_name = org_name.replace("For Supreme Court","")
-        org_name = org_name.replace("for Supreme Court","")
-        reversed = True
-    if "Campaign" in org_name.title():
-        org_name = org_name.replace(" Campaign","")
-        reversed = True
-    if "Citizens For" in org_name.title():
-        org_name = org_name.replace("Citizens for","")
-        reversed = True
-    org_name = org_name.replace(" SPAC ","")
-    org_name = re.sub(r'\(.*\)',"",org_name) # replace anything inside parentheses
-    org_name = org_name.replace(" Jr.","")
-    org_name = org_name.replace("Jr.","")
-    org_name = org_name.replace(" Sr.","")
-    org_name = org_name.replace("Jr","")
-    org_name = org_name.replace("PAC","")
-    org_name = re.sub(r' [A-Za-z]{1,1}\.( |$)'," ",org_name) # replace single letters followed by periods
-    org_name = re.sub(r"\'.*\'","",org_name) # replace anything inside parentheses
-    # org_name = re.sub(r" I{2,3}","",org_name)
-    org_name = re.sub(r' \b(?=[MDCLXVI]+\b)M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})\b',"",org_name) # replace roman numerals
-    org_name = org_name.title()
-    # org_name = org_name.replace("    "," ")
-    # org_name = org_name.replace("  "," ")
-    org_name = re.sub(r' {2,}'," ",org_name)
-    org_name = org_name.strip()
-    org_name = org_name.replace("\"","")
-    if not(reversed) and len(org_name.split(",")) == 2:
-        org_name = " ".join(org_name.split(",")[::-1])
-    if re.match(".* [A-Za-z]{1} .*",org_name):
-        temp = org_name.split(" ")
-        org_name = " ".join([temp[0],temp[1]])
-    return org_name
-
-def merge_data():
-    files = glob.glob("{}/contrib*.csv".format(data_dir))
-    df = pd.concat((pd.read_csv(x,dtype=str) for x in files),ignore_index=True)
-    df.to_csv("../../data/combined.csv")
-    print(files)
-
 if __name__ == "__main__":
 #     print(dat_file)
-    merge_data()
+    # merge_data()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-merge", "--merge",help="merges the contribution data files into one csv",type=str)
+    parser.add_argument("-pnames", "--pnames",help="gets all contributor names",action="store_true")
+    parser.add_argument("-groupby", "--groupby",help="groups the merged dataframe by a column",action="store_true")
+    parser.add_argument("-lookup", "--lookup",help="looks up filer by filer id",type=str,required=False)
+    parser.add_argument("-filerinfo", "--filerinfo",help="looks up filer info by filer id",type=int,required=False, nargs="*")
 
+    args = parser.parse_args()
+    
+    # df = pd.read_csv("../../data/combined_all_preview.csv")
+    df = pd.read_csv("../../data/combined_all.csv")
+    df = df[df["contributorPersentTypeCd"] == "ENTITY"]
+    df["contributorNameOrganization"] = df["contributorNameOrganization"].apply(lambda x: x.strip())
+    pprint(df.drop_duplicates("contributorNameOrganization")["contributorNameOrganization"].values.tolist())
+
+    if args.merge:
+        if args.merge.lower() == "individual":
+            merge_data(filename="individuals_combined.csv",allowed_contributor_types=["INDIVIDUAL"],debug=True)
+        elif args.merge.lower() == "entity":
+            merge_data(filename="entities_combined.csv",allowed_contributor_types=["ENTITY",""],debug=True)
+        elif args.merge.lower() == "all":
+            merge_data(filename="combined_all.csv",allowed_contributor_types=["ENTITY","INDIVIDUAL",""],debug=True)
+
+    if args.pnames:
+        dat = pd.read_csv("../../data/combined.csv")
+        pprint(dat.drop_duplicates("contributorNameOrganization")[["contributorNameOrganization"]].values.tolist())
+
+    if args.groupby:
+        dat = pd.read_csv("../../data/combined.csv")
+        dat = dat.groupby(["filerIdent"]).sum()["contributionAmount"]
+        dat = dat.copy()
+        dat.sort()
+        pprint(dat.to_dict())
+
+    if args.lookup:
+        print(args.lookup,type(args.lookup))
+        dat = pd.read_csv("../../data/combined.csv")
+        print(dat[dat["filerIdent"] == int(args.lookup)]["contributionAmount"].sum())
+
+    if args.filerinfo:
+        pprint(filer_id_lookup(args.filerinfo))
 
---
+++ scripts/scraping/utils.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import csv
+from pprint import pprint
+import sys
+import numpy as np
+import re
+import glob
+
+def extract_filer_name(org_name):
+    reversed = False
+    if "Texans For" in org_name.title():
+        org_name = org_name.replace("Texans for ","")
+        org_name = org_name.replace("Texans for ".upper(),"")
+        reversed = True
+    if "Friends Of" in org_name.title():
+        org_name = org_name.replace("Friends of ","")
+        org_name = org_name.replace("Friends Of ","")
+        org_name = org_name.replace("Friends Of ".upper(),"")
+        reversed = True
+    if "Political Action Committee" in org_name.title():
+        org_name = org_name.replace("Political Action Committee","")
+        reversed = True
+    if "Committee To Elect" in org_name.title():
+        org_name = org_name.replace("Committee to Elect ","")
+    if "Committee" in org_name:
+        org_name = org_name.replace(" Committee","")
+        reversed = True
+    if "The Coalition To Elect" in org_name.title():
+        org_name = org_name.replace("The Coalition to Elect","")
+    if "Coalition To Elect" in org_name.title():
+        org_name = org_name.replace("Coalition to Elect","")
+    if "For Texas Supreme Court" in org_name.title():
+        org_name = org_name.replace(" For Texas Supreme Court","")
+        org_name = org_name.replace("For Texas Supreme Court".upper(),"")
+        reversed = True
+    if "For Texas Supreme Court" in org_name.title():
+        org_name = org_name.replace("for Texas Supreme Court","")
+        reversed = True
+    if "For Texas" in org_name.title():
+        org_name = re.sub(r"(F|f)or.*","",org_name)
+        org_name = org_name.replace("For Texas","")
+        org_name = org_name.replace("for Texas","")
+        reversed = True
+    if "Justice" in org_name.title():
+        org_name = org_name.replace("Justice","")
+        reversed = True
+    if "For Supreme Court" in org_name.title():
+        org_name = org_name.replace("For Supreme Court","")
+        org_name = org_name.replace("for Supreme Court","")
+        reversed = True
+    if "Campaign" in org_name.title():
+        org_name = org_name.replace(" Campaign","")
+        reversed = True
+    if "Citizens For" in org_name.title():
+        org_name = org_name.replace("Citizens for","")
+        reversed = True
+    org_name = org_name.replace(" SPAC ","")
+    org_name = re.sub(r'\(.*\)',"",org_name) # replace anything inside parentheses
+    org_name = org_name.replace(" Jr.","")
+    org_name = org_name.replace("Jr.","")
+    org_name = org_name.replace(" Sr.","")
+    org_name = org_name.replace("Jr","")
+    org_name = org_name.replace("PAC","")
+    org_name = re.sub(r' [A-Za-z]{1,1}\.( |$)'," ",org_name) # replace single letters followed by periods
+    org_name = re.sub(r"\'.*\'","",org_name) # replace anything inside parentheses
+    # org_name = re.sub(r" I{2,3}","",org_name)
+    org_name = re.sub(r' \b(?=[MDCLXVI]+\b)M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})\b',"",org_name) # replace roman numerals
+    org_name = org_name.title()
+    # org_name = org_name.replace("    "," ")
+    # org_name = org_name.replace("  "," ")
+    org_name = re.sub(r' {2,}'," ",org_name)
+    org_name = org_name.strip()
+    org_name = org_name.replace("\"","")
+    if not(reversed) and len(org_name.split(",")) == 2:
+        org_name = " ".join(org_name.split(",")[::-1])
+    if re.match(".* [A-Za-z]{1} .*",org_name):
+        temp = org_name.split(" ")
+        org_name = " ".join([temp[0],temp[1]])
+    return org_name
+data_dir = "../../data/texas_ethics_commission"
+def merge_data(columns=None,by=None,debug=False,filename="combined.csv",allowed_contributor_types=["ENTITY",""],allowed_form_types=["COH"]):
+    files = glob.glob("{}/contrib*.csv".format(data_dir))
+    files_data = []
+    for file in files:
+        dat = pd.read_csv(file,dtype=str)
+        # dat = dat[dat["formTypeCd"] =="COH"].iloc[:500]
+        # dat = dat[(dat["contributorPersentTypeCd"].isin(allowed_contributor_types)) & (dat["formTypeCd"].isin(allowed_form_types))]
+        dat = dat[(dat["contributorPersentTypeCd"].isin(allowed_contributor_types))]
+        dat.drop([
+            "recordType",
+            "schedFormTypeCd",
+            "infoOnlyFlag",
+            "contributorNameSuffixCd",
+            "contributorNamePrefixCd",
+            "contributorStreetCity",
+            "contributorStreetStateCd",
+            "contributorStreetRegion",
+            "contributorOccupation",
+            "contributorOosPacFlag",
+            "contributorSpouseLawFirmName",
+            "contributorParent1LawFirmName",
+            "contributorParent2LawFirmName"],axis=1,inplace=True)
+        files_data.append(dat)
+        if debug: 
+        	print(file)
+
+    df = pd.concat(files_data,ignore_index=True)
+    df.to_csv("../../data/{}".format(filename),index=False)
+    df.iloc[0:200000].to_csv("../../data/{}_preview.csv".format(filename.replace(".csv","")),index=False)
+    return df
+
+def filer_id_lookup(filerinfo):
+    dat = pd.read_csv("{}/filers.csv".format(data_dir))
+    return dat[dat["filerIdent"].isin(filerinfo)][["filerIdent","filerName","filerTypeCd","filerPersentTypeCd"]].to_dict("records")
