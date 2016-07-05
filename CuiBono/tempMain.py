4ec31743c713c4382e165a4408d2c7692e44e59e
13lheytens <lukeheytens@utexas.edu>
2016-07-05 14:58:28 -0500

    Navigatable links in each category

--- CuiBono/tempMain.py
+++ CuiBono/tempMain.py
@@ -48,6 +48,18 @@ def walle():
 def davis():
 	return render_template("davis.html") 
 
+@app.route("/contributors/borderhealthpac")
+def borderhealthpac():
+	return render_template("borderhealthpac.html") 
+
+@app.route("/contributors/ampac")
+def AMpac():
+	return render_template("AMpac.html") 
+
+@app.route("/contributors/plumberspac")
+def plumberspac():
+	return render_template("plumberspac.html") 
+
 if __name__ == "__main__":
 	app.run("0.0.0.0",debug=True)
 
