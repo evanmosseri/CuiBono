9a51e4def5d54f2de3ccad7d4380bd59a527292b
Evan Mosseri <evan.mosseri@gmail.com>
2016-07-05 11:06:56 -0500

    commmit before i go to gdc

--- CuiBono/main.py
+++ CuiBono/main.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/bills")
+@app.route("/bills/")
+@app.route("/bills/<int:billnumber>")
+def bills(billnumber=6):
+	return render_template("bills.html",{"bills":[1,2,3,4]})
 
+@app.route("/legistors")
+@app.route("/legistors/")
+def legislator():
+	return render_template("legistors.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
