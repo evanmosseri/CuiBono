96523664e54cffafa3f6f473bb339f93dadc2be1
yu zhong <williamz@cs.utexas.edu>
2016-07-12 21:41:51 -0500

    Should have all the data in json files

--- CuiBono/wsgi.py
+++ CuiBono/wsgi.py
@@ -1,4 +1,11 @@
 from tempMain import app
+import json
+import requests
+url = "http://openstates.org/api/v1/metadata/tx/"
+params = {"apikey":"3df8f699b8654d5ca218a7db79c8b0c4"}
+resp = requests.get(url=url, params = params)
+text = json.loads(resp.text)
+print (text)
 
 if __name__ == "__main__":
     app.run()
