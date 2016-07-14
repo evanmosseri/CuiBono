31f80af35e6815e3b7c78ff609a80a7768d4dd25
Stewart Johnson <shjohnson01@gmail.com>
2016-07-14 15:00:48 -0500

    Add tempMain progress

--- CuiBono/tempMain.py
+++ CuiBono/tempMain.py
@@ -12,30 +12,40 @@ legislatorz = db.session.query(Legislator).all()
 contributorz = db.session.query(Contributor).all()
 contributionz = db.session.query(Contribution).all()
 
-print(billz[0].sponsors[0].contributions)
-
-
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route("/legislators/<int:id>")
+@app.route("/legislators/<id>")
+def legislator(legislator_id):
+	return render_template("legislator.html", legislator = legislatorz["legislator_id"])
+
 @app.route("/legislators")
 def legislators(id=None):
-	return render_template("legislators.html") 
+	return render_template("legislators.html" legislators = legislatorz) 
 
 @app.route("/bills/<id>")
+def bill(bill_id):
+	return render_template("bill.html", bill = billz["bill_id"])
+
 @app.route("/bills")
 def bills(id=None):	
-	return render_template("bills.html",bills = billz) 
+	return render_template("bills.html", bills = billz) 
 
-@app.route("/contributors/<id>")
+@app.route("/contributors/<int:id>")
+def contributor(contributor_id):
+	return render_template("contributor.html" contributor = contributorz["contributor_id"])
+
 @app.route("/contributors")
 def contributors(id=None):
-	return render_template("contributors.html") 
+	return render_template("contributors.html", contributors = contributorz) 
+
+@app.route("/contributions/<int:id>")
+def contribution(contribution_id):
+	return render_template("contribution.html", contribution = contributionz["contribution_id"])
 
 @app.route("/contributions")
-def contributions():
+def contributions(id=None):
 	return render_template("contributions.html") 	
 
 @app.route("/about")
