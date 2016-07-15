c879a0b909ab095d2755721cc41b9d41b90d58f7
Adam Miller <jimmyne@gmail.com>
2016-07-14 21:14:29 -0400

    Finsihed apiary API

--- CuiBono/tempMain.py
+++ CuiBono/tempMain.py
@@ -75,25 +75,65 @@ def unittest(name = None):
 	file.close()
 	return render_template('unittest.html',name = contents)
 
-@app.route("/api/bill")
+@app.route("/api/bill/")
 def get_all_bills():
 	bills = db.session.query(Bill).all()
-	return jsonify(response=bills)
+	return jsonify([bill_to_dict(bil)for bil in bills])
 
-@app.route("/api/legislator")
+@app.route("/api/legislator/")
 def get_all_legislators():
-	legislator = db.session.query(Legislator).all()
-	return jsonify(response=legislator)
+	legislators = db.session.query(Legislator).all()
+	return jsonify([legis_to_dict(never_skip_leg_day) for never_skip_leg_day in legislators])
 
-@app.route("/api/contributor")
+@app.route("/api/contributor/")
 def get_all_contributor():
-	contributors = db.session.query(Contributor).all()
-	return jsonify(response=contributors)
+	contributors = db.session.query(Contributor).limit(50).all()
+	return jsonify([contributor_to_dict(contra) for contra in contributors])
 
-@app.route("/api/contribution")
-def get_all_legislators():
-	contributions = db.session.query(Contribution).all()
-	return jsonify(response=contributions)
+@app.route("/api/contribution/")
+def get_all_legislator():
+	contributions = db.session.query(Contribution).limit(50).all()
+	return jsonify([contribution_to_dict(x) for x in contributions])
+
+def bill_to_dict(bill):
+	if bill is None:
+		return None
+	bill_temp = {"id" : bill.id, "no_count" : bill.no_count, "no_votes" : bill.no_votes, "yes_count" : bill.yes_count, "yes_votes" : bill.yes_votes, "prefix" : bill.prefix, "number": bill.number, "session": bill.session, "sources": bill.sources,"sponsor_meta" : bill.sponsor_meta, "subjects" : bill.subjects, "title" : bill.title }
+	sponsors_list = []
+	for x in bill.sponsors :
+		sponsors_list.append(legis_to_dict(x))
+	bill_temp["sponsors"] = sponsors_list
+	return bill_temp
+
+def legis_to_dict(legis):
+	if legis is None:
+		return None 
+	legis_dict = {"id" : legis.id, "filer_id" : legis.filer_id, "sources" : legis.sources , "offices" : legis.offices, "first_name" : legis.first_name , "middle_name" : legis.middle_name, "last_name" : legis.last_name, "district" : legis.district, "party" : legis.party, "photo_url" : legis.photo_url}
+	return legis_dict
+
+def contributor_to_dict(contra):
+	if contra is None:
+		return None
+	contra_temp = {"id" : contra.id, "name" : contra.name, "type" : contra.type, "zip" : contra.zip}
+	contribution_list = []
+	for contribution in contra.contributions:
+		contribution_list.append(contribution_to_dict(contribution))
+	contra_temp["contributions"] = contribution_list
+	return contra_temp
+
+def contribution_to_dict(contra):
+	if contra is None:
+		return None
+	contra_temp = {"id" : contra.id, "amount" : contra.amount, "filer_id" : contra.filer_id, "submitted_date" : contra.submitted_date, "contributor_id" : contra.contributor_id, "legislator_id": contra.legislator_id}
+	contributor_list = []
+	legislator_list = []
+
+	contra_temp["contributor"] = contra.contributor.name
+	if contra.legislator is not None:
+		contra_temp["legislator"] = contra.legislator.first_name + " " + contra.legislator.last_name
+	else:
+		contra_temp["legislator"] = None
+	return contra_temp
 
 if __name__ == "__main__":
 	app.run("0.0.0.0",debug=True)
