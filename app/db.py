1632c717ea086b0639c1131265ffed46529aa31d
Evan Mosseri <evan.mosseri@gmail.com>
2016-07-19 13:59:31 -0500

    removed unnecessary files... again

--- app/db.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://:evan@localhost/testdb"
-db = SQLAlchemy(app)
